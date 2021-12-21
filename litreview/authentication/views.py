a8d9994a20a5dd09753f288b589857a49b00d7bd
Orphée <letembeorphee@gmail.com>
2021-12-21 03:02:19 +0100

    update

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from . import forms
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ""
+    if request.method == "POST":
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                message = f"Bonjour, {user.username}! vous êtes connecté"
+            else:
+                message = "Identifiants invalides."
+    return render(request, "authentication/login.html", context={"form": form, "message": message})
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == "POST":
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, "authentication/signup.html", context={"form": form})
