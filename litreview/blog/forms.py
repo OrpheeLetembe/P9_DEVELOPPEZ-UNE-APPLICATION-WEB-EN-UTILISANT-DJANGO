b547a97766816bb4b48e448c7aa5b6b6e1c6bc50
Orphée <letembeorphee@gmail.com>
2021-12-26 00:53:00 +0100

    first commit

---
+++ litreview/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from . import models
+
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        fields = ['titre', 'description', 'image']
+
+
+
+
+
